49a6e06c2f480aac9be4d7266e6e708c4a2fe262
lebrancconvas <lebrancconvas@gmail.com>
2022-05-05 12:52:49 +0700

    ✨ Create Result Component.

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import Heading from './components/Heading';
 import Searching from './components/Searching';
 
+import Result from './components/Result';
+
 function App() {
   return (
     <div>
       <Heading />
       <Searching />
+      <Result />
     </div>
   );
 }
