a4647a7344354f0d0caa46dd49595002a7b0009f
lebrancconvas <lebrancconvas@gmail.com>
2022-05-05 12:52:35 +0700

    🔨 Edit Server Port.

--- api/src/app.ts
+++ api/src/app.ts
@@ -2,11 +2,12 @@ import express from 'express';
 
 const app = express();
 
-const PORT = 8000;
+const PORT = 8002;
 
 app.get('/', (req: express.Request, res: express.Response) => {
 	res.send('Hello World!');
 });
+
 
 app.listen(PORT, () => {
 	console.log(`Listening on PORT: ${PORT}`);
